112c9272bc7ea2fab92a54f8afd5eaa0f3926ee6
Alen <info@alenbarac.com>
2023-05-13 15:27:23 +0200

    data fetch based on btn click

--- src/components/DataFetching.jsx
+++ src/components/DataFetching.jsx
@@ -4,10 +4,15 @@ import axios from 'axios'
 const DataFetching = () => {
   const [post, setPost] = useState({})
   const [id, setId] = useState(1)
+  const [idFromBtn, setIdFromBtn] = useState(1)
+
+  const handleCLick = () => {
+    setIdFromBtn(id)
+  }
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromBtn}`)
       .then((res) => {
         console.log(res)
         setPost(res.data)
@@ -15,10 +20,13 @@ const DataFetching = () => {
       .catch((err) => {
         console.log(err)
       })
-  }, [id])
+  }, [idFromBtn])
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleCLick}>
+        fetch post
+      </button>
       <div>{post.title}</div>
       {/* {posts?.map((post) => (
         <div key={post.id}>
