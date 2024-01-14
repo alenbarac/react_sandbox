e676f9fe4ec421257b9d291c33777dc2ba632dbd
Alen <info@alenbarac.com>
2024-01-14 16:41:22 +0100

    starter project init

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,27 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import StateObject from './components/stateObject'
-import StateArray from './components/stateArray'
-import HookMouse from './components/hookMouse'
-import MouseContainer from './components/MouseContainer'
-import IntervalCount from './components/intervalCount'
-import DataFetching from './components/DataFetching'
-import CounterReducer from './components/CounterReducer'
-import Counter2Reducer from './components/Counter2Reducer'
-import DataFetchReducer from './components/DataFetchReducer'
-import MemoCounter from './components/memoCounter'
-import FocusInput from './components/FocusInput'
-import RefTimer from './components/refTimer'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('')
-
-  const initial_value = 0
-
-  useEffect(() => {
-    console.log('update title value')
-    document.title = `You clicked ${count} times`
-  }, [count])
-
   return (
-    <>
-      <h1>React sandbox</h1>
-      <div className="card">
-        <RefTimer />
-        {/* <FocusInput /> */}
-        {/*  <DataFetchReducer /> */}
-        {/* <Counter2Reducer /> */}
+    <div className="App">
+      <div className="container mx-auto px-4 mt-20 ">
+        <div class="grid grid-cols-12 gap-3">
+          <div class="col-span-5">
+            <div className="card bg-black shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Form</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-7">
+            <div className="card bg-black shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Posts</h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* <div className="card">
-        <IntervalCount />
-      </div> */}
-      {/* <div className="card">
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>increment </button>
-        <button onClick={() => setCount((count) => initial_value)}>Reset</button>
-        <button onClick={() => setCount((count) => count - 1)}>Decrement </button>
-        <div>{count}</div>
-        <hr />
-        STATE OBJECT
-        <StateObject />
-        <hr />
-        STATE ARRAY
-        <StateArray />
-        <MouseContainer />
-      </div> */}
-    </>
+    </div>
   )
 }
 
